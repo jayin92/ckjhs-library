6d80668cdf07dc391f210717aa5a890bc161cee9
jayin92 <jayin920805@gmail.com>
2017-09-10 19:11:38 +0800

    download-this-version

--- code/main.py
+++ code/main.py
@@ -34,74 +34,93 @@ borrowBook = False
 os.system(clean)
 updateSheet()
 mode = 'b'
+conti = ''
 while True:
-	mode = input('輸入b來借書, r來還書 >')
-	print(mode)
+	os.system(clean)
+	if conti == 'y':
+		mode = 'b'
+	else:
+		mode = input('輸入b來借書, r來還書 >')
 	if mode == 'b':
 		if len(userName) == 0:
-			userNum = input('請輸入學號 >')
+			userNum = input('請輸入班級座號(五碼) >')
 			for item in userData:
-				if str(item['學號']) == userNum:
+				if str(item['班級座號']) == userNum:
 					userName = item['姓名']
 					exit_flag = False
 					os.system(clean)
-					break
 		if len(userName) == 0:
-			print("沒有找到您的資料, 請在重新輸入一次")
-			break
-		exit_flag = True
-		print('歡迎'+userName+'同學, 今天要借什麼書呢？')
-		isbn = input("請刷條碼或輸入 q 取消借書 >")
+			print("沒有找到您的資料, 請再重新輸入一次")
+			updateSheet()
+		else:
+			print('歡迎'+userName+'同學, 今天要借什麼書呢？')
+			isbn = input("請刷條碼或輸入 q 取消借書 >")
 		if isbn == 'q':
 			os.system(clean)
 			print("再見"+userName+"同學")
 			updateSheet()
-			os.system(clean)
 			userName = ''
-			break
-
+			isbn = ''
+			# mode = ''
 		userBook = ''
+		row = 1
 		for book in bookData:
 			if str(book['ISBN']) == isbn:
 				userBook = book
-				exit_flag = False
-		if exit_flag is True:
+				break
+			row += 1
+		if len(userBook) == 0:
 			os.system(clean)
 			print('找不到這本書, 請確認ISBN是否正確')
-		if exit_flag is False:
+		elif userBook['借出'] is not None:
+			os.system(clean)
+			print(userBook['書籍名稱']+'以被'+str(userBook['借出'])+'借出')
+			print('請選擇其他書籍 ：）')
+			time.sleep(5)
+		else:
 			os.system(clean)
 			print('書籍名稱: '+userBook['書籍名稱'])
 			print('作者: '+userBook['作者'])
-			check = input('確認借閱請輸入 y , 取消輸入 n > ')
+			check = input('確認借閱請輸入 y , 取消借閱輸入 n > ')
 			if check == 'y':
 				borrowinfo = [userName, userNum, time.strftime(fmt), userBook['書籍名稱'], userBook['ISBN']]
 				os.system(clean)
+				borrowSheet.append_row(borrowinfo)
+				bookSheet.update_cell(row, 5, userNum)
+				updateSheet()
 				print('已成功借書')
 				print('借閱人: '+userName)
 				print('借閱書籍: '+userBook['書籍名稱'])
 				print('借閱時間: '+time.strftime(fmt))
-				borrowSheet.append_row(borrowinfo)
-				updateSheet()
+				conti = input('還有要借書嗎？, 有 請輸入y, 沒有 輸入n >')
+				isbn = ''
+				userBook = ''
+				if not conti == 'y':
+					userName = ''
 			else:
 				os.system(clean)
 				print('已取消')
 	if mode == 'r':
 		rentBook = []
 		if len(userName) == 0:
-			userNum = input('請輸入學號 >')
+			userNum = input('請輸入班級座號(五碼) >')
 		for item in userData:
-			if str(item['學號']) == userNum:
+			if str(item['班級座號']) == userNum:
 				userName = item['姓名']
 				exit_flag = False
 				os.system(clean)
 				break
 		if len(userName) == 0:
-			print("沒有找到您的資料, 請在重新輸入一次")
-			break
-		for item in borrowData:
-			if str(item['借閱人學號']) == userNum:
-				rentBook.append(item)
-		if len(rentBook) == 0:
-			os.system(clean)
-			print('您沒有已借閱的書, 快去借書 ：）')
-		print(rentBook)
+			print("沒有找到您的資料, 請再重新輸入一次")
+		else:
+			for item in borrowData:
+				if str(item['借閱人班級座號']) == userNum:
+					rentBook.append(item['借閱書籍'])
+			if len(rentBook) == 0:
+				os.system(clean)
+				print('您沒有已借閱的書, 先去借書再來吧 ：）')
+			else:
+				print('您借的書如下：')
+				for item in rentBook:
+					print(item)
+				time.sleep(5)
