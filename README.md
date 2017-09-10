6fce539ac4dccd43779bd9360bc7fb89bb57f23e
jayin92 <jayin920805@gmail.com>
2017-09-10 10:19:51 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ckjhs-library
+# 桃園市立建國國中數理資優班圖書管理系統
+## 如何安裝
+若要執行此套件需要三個package, 分別是 oauth2client gspread isbnlib 安裝指令如下：
+
+    pip install oauth2client gspread isbnlib
+
+## 程式說明
--- code/main.py
+++ code/main.py
@@ -1,3 +1,5 @@
+# coding=<UTF-8>
+
 import gspread
 import time
 import isbnlib
@@ -11,6 +13,7 @@ client = gspread.authorize(cred)
 mode = 2
 exit_flag = True
 fmt = '%Y/%m/%d %H:%M:%S'
+isbn = ''
 # borrowSheet.resize(1)
 borrowSheet = client.open('圖書總表').worksheet('借閱總表')
 userSheet = client.open('圖書總表'). worksheet('借閱人總表')
@@ -18,24 +21,29 @@ bookSheet = client.open('圖書總表'). worksheet('書籍總表')
 userData = userSheet.get_all_records()
 bookData = bookSheet.get_all_records()
 userName = ''
+borrowBook = False
 while True:
-	userNum = int(input('請輸入學號 >'))
-	if len(userName) == 0:
-		for item in userData:
-			if item['學號'] == userNum:
-				userName = item['姓名']
-				exit_flag = False
-				break
-	if exit_flag is True:
-		print("沒有找到您的資料, 請在重新輸入一次")
-
-	if exit_flag is False:
+	while True:
+		if len(userName) == 0:
+			userNum = int(input('請輸入學號 >'))
+			for item in userData:
+				if item['學號'] == userNum:
+					userName = item['姓名']
+					exit_flag = False
+					break
+		if len(userName) == 0:
+			print("沒有找到您的資料, 請在重新輸入一次")
+			break
 		exit_flag = True
-		print('歡迎'+userName+'同學, 今天要借什麼書呢？')
+		if borrowBook is False:
+			print('歡迎'+userName+'同學, 今天要借什麼書呢？')
 		isbn = int(input("請刷條碼或輸入q取消借書 >"))
 		if isbn == 10:
 			print("再見"+userName+"同學")
-			exit_flag = True
+			userName = ''
+			borrowBook = False
+			break
+
 		userBook = ''
 		for book in bookData:
 			if book['ISBN'] == isbn:
@@ -43,9 +51,10 @@ while True:
 				exit_flag = False
 		if exit_flag is True:
 			print('找不到這本書, 請確認ISBN是否正確')
+			borrowBook = True
 		if exit_flag is False:
-			print(userBook)
 			print('書籍名稱: '+userBook['書籍名稱'])
+			print('作者: '+userBook['作者'])
 			check = input('確認借閱請輸入y, 取消輸入n')
 			if check == 'y':
 				borrowinfo = [userName, time.strftime(fmt), userBook['書籍名稱'], userBook['ISBN']]
